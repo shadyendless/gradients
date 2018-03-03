a3009d29f672790c3254e37f983f99b916013a07
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-03-03 01:54:50 -0800

    Removed some files and masked the awkward flash with an overlay on page load.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,73 +1,22 @@
 import Helmet from 'react-helmet'
-import tinycolor from 'tinycolor2';
-import React, { Component } from 'react'
-import Navigation from '../components/Navigation';
-import GradientCode from '../components/GradientCode';
-import GradientHeader from '../components/GradientHeader';
-import { calculateTextColor, calculateGradient, getInitialBackgroundColor, getRandomColor, toFormatString } from '../utils';
+import React, { Fragment } from 'react'
 
 import '../sass/main.scss';
 
-class TemplateWrapper extends Component {
-  state = {
-    backgroundColor: null
-  };
-
-  setBackgroundColor = (color) => {
-    this.setState(() => ({
-      backgroundColor: color
-    }));
-  };
-
-  randomizeBackgroundColor = () => {
-    const formatString = toFormatString(tinycolor(this.state.backgroundColor).getFormat());
-    this.setState((prevState, props) => ({
-      backgroundColor: tinycolor.random()[formatString]()
-    }));
-  };
-
-  componentDidMount = () => {
-    document.addEventListener('keyup', (event) => {
-      if (event.which === 82) {
-        this.randomizeBackgroundColor();
-      }
-    });
-
-    if (window.location.hash !== '')
-      this.setBackgroundColor(getInitialBackgroundColor(window.location.hash));
-  };
-
-  render() {
-    const { children } = this.props;
-    let { backgroundColor } = this.state;
-
-    if (backgroundColor === null) {
-      this.setBackgroundColor(getRandomColor());
-    }
-
-    const navColor = calculateTextColor(backgroundColor || 'rgb(255, 255, 255)');
-    const gradient = calculateGradient(backgroundColor || 'rgb(255, 255, 255)');
-
-    return (
-      <div className="site-wrapper is-flex flex-column">
-        <Helmet
-          title="Gradients by Jacob Foster"
-          meta={[
-            { name: 'description', content: 'Create, share, and discover great looking gradients to use in your projects.' },
-            { name: 'keywords', content: 'gradients, generator, good, modern, share, save' },
-          ]}
-        />
-        <Navigation fontColor={navColor} />
-        <GradientHeader gradient={gradient}
-          backgroundColor={backgroundColor || 'rgb(255, 255, 255)'}
-          navColor={navColor}
-          setBackgroundColor={this.setBackgroundColor}
-          randomizeBackgroundColor={this.randomizeBackgroundColor} />
-        <GradientCode gradient={gradient} />
-        {children()}
-      </div>
-    );
-  }
-}
+const TemplateWrapper = ({ children }) => (
+  <Fragment>
+    <Helmet
+      title="Gradients by Jacob Foster"
+      meta={[
+        { name: 'description', content: 'Create, share, and discover great looking gradients to use in your projects.' },
+        { name: 'keywords', content: 'gradients, generator, good, modern, share, save' },
+        { charset: 'utf-8' },
+        { 'http-equiv': 'X-UA=Compatible', content: 'IE=edge' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+      ]}
+    />
+    {children()}
+  </Fragment>
+);
 
 export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import tinycolor from 'tinycolor2';
+import React, { Component } from 'react'
+import Navigation from '../components/Navigation';
+import GradientCode from '../components/GradientCode';
+import GradientHeader from '../components/GradientHeader';
+import { calculateTextColor, calculateGradient, getInitialBackgroundColor, getRandomColor, toFormatString } from '../utils';
 
-const IndexPage = () => (
-  <div>
-  </div>
-);
+class IndexPage extends Component {
+  state = {
+    backgroundColor: getRandomColor()
+  };
+
+  setBackgroundColor = (color) => {
+    this.setState(() => ({
+      backgroundColor: color
+    }));
+  };
+
+  randomizeBackgroundColor = () => {
+    const formatString = toFormatString(tinycolor(this.state.backgroundColor).getFormat());
+    this.setState((prevState, props) => ({
+      backgroundColor: tinycolor.random()[formatString]()
+    }));
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event) => {
+      if (event.which === 82) {
+        this.randomizeBackgroundColor();
+      }
+    });
+
+    if (window.location.hash !== '')
+      this.setBackgroundColor(getInitialBackgroundColor(window.location.hash));
+  };
+
+  render() {
+    const { children } = this.props;
+    const { backgroundColor } = this.state;
+    const navColor = calculateTextColor(backgroundColor);
+    const gradient = calculateGradient(backgroundColor);
+
+    return (
+      <div className="site-wrapper is-flex flex-column">
+        <Navigation fontColor={navColor} />
+        <GradientHeader gradient={gradient}
+          backgroundColor={backgroundColor}
+          navColor={navColor}
+          setBackgroundColor={this.setBackgroundColor}
+          randomizeBackgroundColor={this.randomizeBackgroundColor} />
+        <GradientCode gradient={gradient} />
+      </div>
+    );
+  }
+}
 
 export default IndexPage
