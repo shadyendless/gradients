5eb9941d8b4014b21cb840d287c92a3a285baa7a
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-03-03 03:06:55 -0800

    Finished up the footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,14 +2,16 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const Footer = ({ gradient, navColor }) => (
-    <div className="footer container">
-        <a className="footer-link" href="https://github.com/shadyendless/gradients">
-            <i className="fab fa-github"></i>&nbsp;&nbsp;&nbsp;View on Github
-        </a>
-        <a className="footer-link" href="https://www.paypal.me/imjacobf">
-            <i className="fab fa-paypal"></i>&nbsp;&nbsp;&nbsp;Found This Useful?
-        </a>
-    </div>
+    <Fragment>
+        <div className="footer container">
+            <a className="footer-link" href="https://github.com/shadyendless/gradients">
+                <i className="fab fa-github"></i>&nbsp;&nbsp;&nbsp;View on Github
+            </a>
+        </div>
+        <div className="notice">
+            Based on <a href="https://medium.com/the-mvp/finally-a-definitive-way-to-make-gradients-beautiful-6b27af88f5f">this article</a> by <a href="https://twitter.com/renebrandel?lang=en">@renebrandel</a>.
+        </div>
+    </Fragment>
 );
 Footer.propTypes = {
     gradient: PropTypes.string.isRequired,
