bd5423142f3409819f8369579b392e1b6ab6e8fb
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-03-03 15:43:28 -0800

    Added more examples to the in use section and cleaned up the CSS.

--- src/components/InUse/index.js
+++ src/components/InUse/index.js
@@ -6,23 +6,38 @@ import me from '../../images/me.jpg';
 const InUse = ({ gradient, navColor }) => (
     <>
         <h1 class="header">In Use</h1>
-        <div class="in-use-wrapper container" style={{ color: navColor }}>
-            <div class="in-use-section">
-                <div class="in-use-content label" style={{ background: gradient }}>
-                    Label
+        <div>
+            <div class="in-use-wrapper container" style={{ color: navColor }}>
+                <div class="in-use-section">
+                    <div class="in-use-content label" style={{ background: gradient }}>
+                        NEW
+                    </div>
+                    <h2 class="in-use-label">Label</h2>
                 </div>
-                <h2 class="subheader">Label</h2>
-            </div>
-            <div class="in-use-section">
-                <div class="in-use-content button" style={{ background: gradient }}>
-                    A Button
+                <div class="in-use-section">
+                    <div class="in-use-content button" style={{ background: gradient }}>
+                        Sign Up
+                    </div>
+                    <h2 class="in-use-label">Button</h2>
                 </div>
-                <h2 class="subheader">Button</h2>
-            </div>
-            <div class="in-use-section">
-                <div class="in-use-content image" style={{ background: `${gradient}, url(${me})`, backgroundSize: 'cover' }}>
+                <div class="in-use-section">
+                    <div class="in-use-content image" style={{ background: `${gradient}, url(${me})`, backgroundSize: 'cover' }}>
+                    </div>
+                    <h2 class="in-use-label">Image</h2>
                 </div>
-                <h2 class="subheader">Image</h2>
+                <div class="in-use-section wide">
+                    <div class="in-use-content message" style={{ background: gradient }}>
+                        Hey! Are you free tonight? We are going out for dinner.
+                    </div>
+                    <h2 class="in-use-label">Message</h2>
+                </div>
+                <div class="in-use-section wide">
+                    <div class="in-use-content status" style={{ background: gradient }}>
+                        Wow!<br />
+                        Did you guys notice that everyone is putting gradients behind their posts? What's the point?
+                    </div>
+                    <h2 class="in-use-label">Post</h2>
+                </div>
             </div>
         </div>
     </>
