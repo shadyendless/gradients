023ab853c3bcdbdbb7fc4c299a39b0cde6105fe3
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-03-04 14:27:31 -0800

    The input border will now either be light or dark depending on the text color.

--- src/components/GradientDisplay/index.js
+++ src/components/GradientDisplay/index.js
@@ -5,11 +5,11 @@ const GradientDisplay = ({ backgroundColor, isFixed, gradient, navColor, randomi
     <div class={`gradient-display ${isFixed ? 'fixed' : ''}`} style={{ background: gradient }}>
         <h1 class="input-header" style={{ color: navColor }}>Edit Color</h1>
         <div class="input-wrapper">
-            <input spellCheck="false" autoComplete="false" class="color-input" style={{ color: navColor }} type="text" value={backgroundColor} onChange={(event) => setBackgroundColor(event.target.value)} />
+            <input spellCheck="false" autoComplete="false" class={`color-input ${navColor === 'rgb(255, 255, 255)' ? 'light' : 'dark'}`} style={{ color: navColor }} type="text" value={backgroundColor} onChange={(event) => setBackgroundColor(event.target.value)} />
             <button class="randomize-color" style={{ color: navColor }} onClick={randomizeBackgroundColor}>
                 <i class="fas fa-random"></i>
             </button>
-            <CopyButton copyText={`https://gradients.jacob-foster.com/?${encodeURIComponent(backgroundColor.replace(/\s/g, '').replace(/\#/g, ''))}`}
+            <CopyButton copyText={`https://gradients.jacob-foster.com/?${encodeURIComponent(backgroundColor.replace(/\s/g, '').replace(/#/g, ''))}`}
                 tooltipText="Link Copied!"
                 btnClass="share-button"
                 btnColor={navColor}>
